cadc4f74bd438581aca41e05bf9687eea1ca2e42
cesagui <cesar.aguirre.work@gmail.com>
2026-05-09 15:23:21 -0700

    add wheel + character selector

---
+++ gester-web-app/src/App.tsx
@@ -0,0 +1,11 @@
+import DonutSelector from './components/DonutSelector'
+
+function App() {
+  return (
+    <div className="w-full min-h-screen">
+      <DonutSelector />
+    </div>
+  )
+}
+
+export default App
